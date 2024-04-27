870c907d09e7e7b1efd6f6c8e033e269440c7e61
oğuz alp çepni <oguzalpcepni@gmail.com>
2024-04-27 13:45:57 +0300

    added modelp mapper servise in core package

--- ProductService/src/main/java/com/productservice/productservice/ProductServiceApplication.java
+++ ProductService/src/main/java/com/productservice/productservice/ProductServiceApplication.java
@@ -1,13 +1,19 @@
 package com.productservice.productservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProductServiceApplication.class, args);
     }
-
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 }
---
+++ ProductService/src/main/java/com/productservice/productservice/core/mapper/ModelMapperManager.java
@@ -0,0 +1,23 @@
+package com.productservice.productservice.core.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+public class ModelMapperManager implements ModelMapperService{
+    private ModelMapper modelMapper;
+    @Override
+    public ModelMapper forResponse() {
+        this.modelMapper.getConfiguration()
+                .setAmbiguityIgnored(true)
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return this.modelMapper;
+    }
+
+    @Override
+    public ModelMapper forRequest() { // request ile gelen her şey maplansin
+        this.modelMapper.getConfiguration()
+                .setAmbiguityIgnored(true)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+        return this.modelMapper;
+    }
+}
